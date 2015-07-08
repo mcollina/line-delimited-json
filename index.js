411c5ea6e46f330423fff69fbc183d6d7e594e0a
Stefan Thies <s.thies@bigdata-analyst.de>
2015-07-08 13:28:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ var split     = require('split2')
   , duplexer  = require('reduplexer')
   , through   = require('through2')
 
+function jsonParse (line)
+{
+  var rv = {}
+  try {
+    rv = JSON.parse(line)
+  } catch (err) {
+    rv.err = err 
+  }
+}
+
 function lineDelimitedJSON(duplex) {
 
-  var readable  = duplex.pipe(split(JSON.parse))
+  var readable  = duplex.pipe(split(jsonParse))
     , writable  = through({ objectMode: true }, function(chunk, enc, cb) {
                     this.push(JSON.stringify(chunk), 'utf8')
                     this.push('\n', 'utf8')
