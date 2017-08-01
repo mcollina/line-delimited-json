9dbef5543a83466213fc9d6bedaf19472e0d3b9b
Matteo Collina <hello@matteocollina.com>
2017-08-01 14:32:33 +0200

    Updated dependencies and standardized.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
+'use strict'
 
-var split     = require('split2')
-  , duplexer  = require('reduplexer')
-  , through   = require('through2')
+var split = require('split2')
+var duplexer = require('reduplexer')
+var through = require('through2')
 
-function jsonParse (line)
-{
+function jsonParse (line) {
   var rv = {}
   try {
     rv = JSON.parse(line)
   } catch (err) {
-    rv.err = err 
+    rv.err = err
     rv.line = line
   }
   return rv
 }
 
-function lineDelimitedJSON(duplex) {
+function lineDelimitedJSON (duplex) {
+  var readable = duplex.pipe(split(jsonParse))
+  var writable = through({ objectMode: true }, function (chunk, enc, cb) {
+    this.push(JSON.stringify(chunk), 'utf8')
+    this.push('\n', 'utf8')
 
-  var readable  = duplex.pipe(split(jsonParse))
-    , writable  = through({ objectMode: true }, function(chunk, enc, cb) {
-                    this.push(JSON.stringify(chunk), 'utf8')
-                    this.push('\n', 'utf8')
-
-                    cb()
-                  })
+    cb()
+  })
 
   writable.pipe(duplex)
 
--- test.js
+++ test.js
@@ -1,70 +1,71 @@
+'use strict'
 
-var test    = require('tap').test
-  , ldj     = require('./')
-  , through = require('through2')
-  , net     = require('net')
-  , counter = 9999
+var test = require('tap').test
+var ldj = require('./')
+var through = require('through2')
+var net = require('net')
+var counter = 9999
 
-function nextPort() {
+function nextPort () {
   return counter++
 }
 
-function ps() {
-  return through(function(chunk, enc, cb) {
+function ps () {
+  return through(function (chunk, enc, cb) {
     this.push(chunk)
     cb()
   })
 }
 
-test('ldj sends objects through the line delimited json stream', function(t) {
+test('ldj sends objects through the line delimited json stream', function (t) {
   t.plan(1)
 
-  var binary    = ps()
-    , instance  = ldj(binary)
-    , msg       = { hello: 'world' }
+  var binary = ps()
+  var instance = ldj(binary)
+  var msg = { hello: 'world' }
 
-  instance.on('data', function(chunk) {
+  instance.on('data', function (chunk) {
     t.deepEqual(chunk, msg)
   })
 
   instance.end(msg)
 })
 
-test('ldj works with net streams', function(t) {
+test('ldj works with net streams', function (t) {
   t.plan(2)
 
-  function handler(client) {
+  function handler (client) {
     var wrappedClient = ldj(client)
-    wrappedClient.on('data', function(data) {
+    wrappedClient.on('data', function (data) {
       t.deepEqual(data, { request: 'message' })
       wrappedClient.end({ response: '42' })
     })
   }
 
-  var server  = net.createServer(handler)
-    , port    = nextPort()
+  var server = net.createServer(handler)
+  var port = nextPort()
 
   server.listen(port)
-  server.on('listening', function() {
+  server.on('listening', function () {
     var client = ldj(net.connect(port))
 
     client.write({ request: 'message' })
 
-    client.on('data', function(data) {
+    client.on('data', function (data) {
       t.deepEqual(data, { response: '42' })
       server.close()
     })
   })
 })
 
-test('ldj accepts plain strings / invalid json', function(t) {
+test('ldj accepts plain strings / invalid json', function (t) {
   t.plan(1)
 
-  var binary    = ps()
-    , instance  = ldj(binary)
-    , msg       = 'test'
+  var binary = ps()
+  var instance = ldj(binary)
+  var msg = 'test'
 
-  instance.on('data', function(chunk) {
+  instance.on('data', function (chunk) {
     t.deepEqual(chunk, msg)
   })
 
